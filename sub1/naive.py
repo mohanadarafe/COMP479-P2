13c220875fc26b18d1ff773054d237b745484d74
mohanadarafe <marafe@live.com>
2020-10-11 00:17:18 -0400

    sub task 1 setup

---
+++ sub1/naive.py
@@ -0,0 +1,12 @@
+from nltk import word_tokenize
+import glob, os, sys
+from bs4 import BeautifulSoup
+
+
+# The following module produces term-docID pairs as it processes documents.
+# Input: Tokens list
+# Output: F list of term-docID pairs
+def task1(tokens):
+    F = []
+    
+    return F

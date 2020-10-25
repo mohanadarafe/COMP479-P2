142c078933dcc7c3c345d791e39a2aa6587e3a87
mohanadarafe <marafe@live.com>
2020-10-25 01:59:31 -0400

    refactor subproject 2

--- subproject2/queryHelper.py
+++ subproject2/queryHelper.py
@@ -1,19 +1,21 @@
-import sub1_modules, json
+import json, os
 
-def queryProcessor(path, query):
+def queryProcessor(query):
+    PATH = "output/sampleQueries.json"
     assert len(query) == 1, "Enter only one query term!"
     query_term = query[0].replace(" ", "")
     dictionary = dict()
 
-    with open(path, 'r') as json_file: 
+    with open("output/postings_list.json", 'r') as json_file: 
         dictionary = json.load(json_file)
 
     assert type(dictionary) == dict, "The input maybe not the right one. Make sure you import the postings list!"
     
-    with open("output/sampleQueries.json", 'a', encoding="utf-8") as outputFile:
+    with open(PATH, 'a', encoding="utf-8") as outputFile:
         if query_term not in dictionary:
             print("The query term you input is not in the corpus!")
             exit()
-        
+
         output = {query_term: dictionary[query_term]}
         json.dump(output, outputFile, indent=3)
+        
---
+++ subproject2/queryProcessor.py
@@ -0,0 +1,22 @@
+'''
+The following block executes the single term query
+
+    QUERY PROCESSOR
+    python subproject2/queryProcessor.py -q "<query>"
+    python subproject2/queryProcessor.py -q "apple"
+    
+    <query>: token to query
+'''
+import queryHelper, time, json, sys
+from tqdm import tqdm
+sys.path.append('utilities')
+import asserts
+
+# Read command line arguments
+args = asserts.init_params()
+query = args.query
+
+start = time.time()
+queryHelper.queryProcessor(query)
+end = time.time()
+print(f'\nDone! Your query was found in {"{:.3f}".format(end-start)} seconds')
